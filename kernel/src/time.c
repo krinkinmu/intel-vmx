fe5cecdb9f8f38d51531a17e4488546ab8512574
Mike Krinkin <krinkin.m.u@gmail.com>
2016-08-27 18:33:55 +0300

    reoarganize timers logic

--- kernel/src/time.c
+++ kernel/src/time.c
@@ -2,6 +2,8 @@
 #include <acpi.h>
 #include <debug.h>
 #include <stdint.h>
+#include <limits.h>
+#include <balloc.h>
 #include <cpu.h>
 
 
@@ -14,73 +16,185 @@ struct acpi_hpet {
 	uint8_t attrs;
 } __attribute__((packed));
 
-static unsigned long long hpet_period;
-static unsigned long hpet_addr;
+struct timer {
+	unsigned long long period;
+	unsigned long long overflow;
+	uintptr_t addr;
+	int cnt_width;
+	int cmp_width;
+	int index;
+};
 
-static unsigned long hpet_read(int reg)
+static struct timer *timers;
+static int timers_size;
+static struct timer *delay_timer;
+
+static unsigned long hpet_read(uintptr_t addr, int reg)
 {
-	volatile uint32_t *ptr = (volatile uint32_t *)(hpet_addr + reg);
+	BUG_ON(reg % 4 != 0);
+	volatile uint32_t *ptr = (volatile uint32_t *)(addr + reg);
 
 	return *ptr;
 }
 
-static unsigned long long hpet_read64(int reg)
+static unsigned long long hpet_read64(uintptr_t addr, int reg)
 {
-	volatile uint64_t *ptr = (volatile uint64_t *)(hpet_addr + reg);
+	BUG_ON(reg % 8 != 0);
+	volatile uint64_t *ptr = (volatile uint64_t *)(addr + reg);
 
 	return *ptr;
 }
 
-static void hpet_write(int reg, unsigned long val)
+static void hpet_write(uintptr_t addr, int reg, unsigned long val)
 {
-	volatile uint32_t *ptr = (volatile uint32_t *)(hpet_addr + reg);
+	BUG_ON(reg % 4 != 0);
+	volatile uint32_t *ptr = (volatile uint32_t *)(addr + reg);
 
 	*ptr = val;
 }
 
-static void hpet_enumerate(void)
+/*
+static void hpet_write64(uintptr_t addr, int reg, unsigned long long val)
 {
-	ACPI_TABLE_HEADER *table;
-	ACPI_STATUS status = AcpiGetTable("HPET", 1, &table);
+	BUG_ON(reg % 8 != 0);
+	volatile uint64_t *ptr = (volatile uint64_t *)(addr + reg);
 
-	if (ACPI_FAILURE(status))
-		BUG("Failed to get HPET table\n");
+	*ptr = val;
+}
+*/
 
-	const struct acpi_hpet *hpet = (const struct acpi_hpet *)table;
+static void hpet_realloc(int new_size)
+{
+	if (new_size <= timers_size)
+		return;
 
-	BUG_ON(hpet->addr.SpaceId != 0);
-	hpet_addr = (unsigned long)hpet->addr.Address;
+	struct timer *old_timers = timers;
+
+	timers = (struct timer *)balloc_alloc(sizeof(*old_timers) * new_size,
+				/* from = */0x1000, /* to = */UINTPTR_MAX);
+	BUG_ON((uintptr_t)timers == UINTPTR_MAX);
+
+	memcpy(timers, old_timers, sizeof(*old_timers) * timers_size);
+	balloc_free((uintptr_t)old_timers,
+				(uintptr_t)(old_timers + timers_size));
+	timers_size = new_size;
+}
+
+static unsigned long long hpet_overflow(int width, unsigned long long period)
+{
+	BUG_ON(width != 32 && width != 64);
+
+	/* overflow period in us is calculated as:
+	 * 2^(width - 1) * period * 10^(-18) * 10^6
+	 * = 2^(width - 1) * period * (2*5)^(-12)
+	 * = 2^(width - 1) * period / (2*5)^12
+	 * = 2^(width - 13) * period / 5^12 */
+	if (width == 64)
+		return 9223372ull * period;
+	return (period << 19) / 244140625;
+}
+
+static void hpet_block_setup(uintptr_t addr)
+{
+	const unsigned long long gen_cap = hpet_read64(addr, 0);
+	const unsigned long period = gen_cap >> 32;
+	const int cnt_width = (gen_cap ^ (1 << 13)) ? 64 : 32;
+	const unsigned long long overflow = hpet_overflow(cnt_width, period);
+	const int count = 1 + ((gen_cap >> 8) & 0xf);
+	const int index = timers_size;
+
+	hpet_realloc(timers_size + count);
+
+	for (int i = 0; i != count; ++i) {
+		struct timer *timer = &timers[index + i];
+		const int conf_reg = 0x100 + 0x20 * i;
+		const unsigned long timer_conf = hpet_read(addr, conf_reg);
+		const int cmp_width = timer_conf & (1 << 5) ? 64 : 32;
+
+		if (timer_conf & (1 << 2))
+			hpet_write(addr, conf_reg, timer_conf & ~(1ul << 2));
+
+		printf("HPET timer width %d, period %lu fs\n",
+					cmp_width, period);
+		timer->period = period;
+		timer->overflow = overflow;
+		timer->addr = addr;
+		timer->cnt_width = cnt_width;
+		timer->cmp_width = cmp_width;
+		timer->index = i;
+	}
+
+	hpet_write(addr, 0x10, 1);
+	if (!delay_timer && cnt_width == 64)
+		delay_timer = &timers[index];
 }
 
 static void hpet_setup(void)
 {
-	BUG_ON((hpet_read(0x00) & (1 << 13)) == 0);
-	hpet_period = hpet_read(0x04);
-	hpet_write(0x10, 1);
+	ACPI_TABLE_HEADER *table;
+	ACPI_STATUS status;
+	int i = 1;
+
+	while ((status = AcpiGetTable("HPET", i++, &table)) != AE_NOT_FOUND) {
+		if (ACPI_FAILURE(status))
+			BUG("Failed to get HPET table\n");
+
+		const struct acpi_hpet *hpet = (const struct acpi_hpet *)table;
+
+		BUG_ON(hpet->addr.SpaceId != 0);
+		hpet_block_setup(hpet->addr.Address);
+	}
+	BUG_ON(!delay_timer);
+}
+
+static void wait_loop(const struct timer *timer, unsigned long long from,
+			unsigned long long until)
+{
+	unsigned long long clk;
+
+	if (until > from) {
+		while (1) {
+			clk = hpet_read64(timer->addr, 0xf0);
+			if (clk > until || clk < from)
+				break;
+			cpu_relax();
+		}
+	} else {
+		while (1) {
+			clk = hpet_read64(timer->addr, 0xf0);
+			if (clk < from && clk > until)
+				break;
+			cpu_relax();
+		}
+	}
+}
+
+static void __udelay(const struct timer *timer, unsigned long usec)
+{
+	const unsigned long long mask = (1ull << (timer->cnt_width - 1));
+	unsigned long long start = hpet_read64(timer->addr, 0xf0);
+
+	while (usec >= timer->overflow) {
+		wait_loop(timer, start, start ^ mask);
+		start ^= mask;
+		usec -= timer->overflow;
+	}
+
+	if (!usec)
+		return;
+
+	const unsigned long long period = timer->period;
+	const unsigned long long clks = (usec * 1000000000ull) / period;
+
+	wait_loop(timer, start, start + clks);
 }
 
 void udelay(unsigned long usec)
 {
-	const unsigned long long clks = (usec * 1000000000ull) / hpet_period;
-	const unsigned long long start = hpet_read64(0xf0);
-	const unsigned long long until = start + clks;
-
-	while (1) {
-		const unsigned long long clk = hpet_read64(0xf0);
-
-		if (until < start) {
-			if (clk <= start && clk >= until)
-				break;
-		} else {
-			if (clk >= until || clk <= start)
-				break;
-		}
-		cpu_relax();
-	}
+	__udelay(delay_timer, usec);
 }
 
 void time_setup(void)
 {
-	hpet_enumerate();
 	hpet_setup();
 }
