443065fd72e922634cc8c2444f30ca9a469ce6b0
Mike Krinkin <krinkin.m.u@gmail.com>
2016-08-29 21:33:08 +0300

    take macro argument in parentheses to avoid questions

--- kernel/inc/apic.h
+++ kernel/inc/apic.h
@@ -13,27 +13,27 @@
 #define APIC_ICR_LEVEL		(1ul << 15)
 #define APIC_ICR_EDGE		(0ul << 15)
 
-#define IO_APIC_VECTOR(x)	(x & 0xfful)
+#define IO_APIC_VECTOR(x)	((x) & 0xfful)
 
-#define IO_APIC_DELIVERY_MODE(x)	((x & 0x7ul) << 8)
+#define IO_APIC_DELIVERY_MODE(x)	(((x) & 0x7ul) << 8)
 #define IO_APIC_FIXED			IO_APIC_DELIVERY_MODE(0)
 #define IO_APIC_LOWEST			IO_APIC_DELIVERY_MODE(1)
 
-#define IO_APIC_DESTINATION_MODE(x)	((x & 0x1ul) << 11)
+#define IO_APIC_DESTINATION_MODE(x)	(((x) & 0x1ul) << 11)
 #define IO_APIC_PHYSICAL		IO_APIC_DESTINATION_MODE(0)
 #define IO_APIC_LOGICAL			IO_APIC_DESTINATION_MODE(1)
 
-#define IO_APIC_POLARITY(x)	((x & 0x1ul) << 13)
+#define IO_APIC_POLARITY(x)	(((x) & 0x1ul) << 13)
 #define IO_APIC_ACTIVE_HIGH	IO_APIC_POLARITY(0)
 #define IO_APIC_ACTIVE_LOW	IO_APIC_POLARITY(1)
 
-#define IO_APIC_TRIGGER(x)	((x & 0x1ul) << 15)
+#define IO_APIC_TRIGGER(x)	(((x) & 0x1ul) << 15)
 #define IO_APIC_EDGE		IO_APIC_TRIGGER(0)
 #define IO_APIC_LEVEL		IO_APIC_TRIGGER(1)
 
 #define IO_APIC_MASK_PIN	(1ul << 16)
 
-#define IO_APIC_DESTINATION(x)	((x & 0xfful) << 24)
+#define IO_APIC_DESTINATION(x)	(((x) & 0xfful) << 24)
 #define IO_APIC_RDREG(x)	(0x10 + 2 * (x))
 #define IO_APIC_RDREG_LOW(x)	IO_APIC_RDREG(x)
 #define IO_APIC_RDREG_HIGH(x)	(IO_APIC_RDREG(x) + 1)
