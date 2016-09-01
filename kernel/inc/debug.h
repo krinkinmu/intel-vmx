a8af1e810abb5484ffd2a9f4e8c02e9c40677eca
Mike Krinkin <krinkin.m.u@gmail.com>
2016-09-01 19:42:31 +0300

    disable interrups in BUG

--- kernel/inc/debug.h
+++ kernel/inc/debug.h
@@ -2,9 +2,11 @@
 #define __DEBUG_H__
 
 #include <print.h>
+#include <ints.h>
 
 #define BUG(...) \
 	do { \
+		local_int_disable(); \
 		printf("BUG at %s:%d: ", __FILE__, __LINE__); \
 		printf(__VA_ARGS__); \
 		while (1); \
