7a1c002fe078d1c1900f7bc21e3db73742ac347d
Mike Krinkin <krinkin.m.u@gmail.com>
2016-09-15 14:31:03 +0300

    remove dummy tests

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -21,72 +21,39 @@ static void acpi_early_setup(void)
 		BUG("Failed to initialize ACPICA tables subsytem\n");
 }
 
-static void guest_entry(void)
-{
-	printf("Guest started.\n");
-	while (1)
-		printf("Guest working...\n");
-}
-
-static void mem_cache_test(void)
-{
-	struct mem_cache cache;
-	struct list_head lst;
-
-	mem_cache_setup(&cache, sizeof(lst), sizeof(lst));
-	list_init(&lst);
-	for (size_t i = 0; i != 1000000; ++i) {
-		struct list_head *ptr = mem_cache_alloc(&cache);
-
-		if (!ptr) {
-			printf("allocated %lu entries\n", (unsigned long)i);
-			break;
-		}
-		list_add_tail(ptr, &lst);
-	}
-
-	for (struct list_head *ptr = lst.next; ptr != &lst;) {
-		void *to_free = ptr;
-
-		ptr = ptr->next;
-		mem_cache_free(&cache, to_free);
-	}
-	mem_cache_release(&cache);
-}
-
-void main(const struct mboot_info *info)
+static void gdb_hang(void)
 {
 #ifdef DEBUG
 	static volatile int wait = 1;
 	while (wait);
 #endif
+}
 
+static void gdt_boot_cpu_setup(void)
+{
+	struct desc_ptr ptr;
+
+	gdt_cpu_create(&ptr);
+	gdt_cpu_setup(&ptr);
+}
+
+void main(const struct mboot_info *info)
+{
+	gdb_hang();
 	uart8250_setup();
 	balloc_setup(info);
+	gdt_boot_cpu_setup();
 	acpi_early_setup();
 	apic_setup();
 	ints_setup();
 	ints_cpu_setup();
 	local_int_enable();
-	tss_setup();
-	tss_cpu_setup();
 	time_setup();
 	time_cpu_setup();
 	smp_early_setup();
 	page_alloc_setup();
-
-	mem_cache_test();
-
 	smp_setup();
 	vmx_setup();
-	vmx_enter();
-	
-	struct vmx_guest guest;
-	static unsigned char stack[4096];
 
-	vmx_guest_setup(&guest);
-	guest.entry = (uintptr_t)&guest_entry;
-	guest.stack = (uintptr_t)(stack + 4096);
-	vmx_guest_run(&guest);
 	while (1);
 }
