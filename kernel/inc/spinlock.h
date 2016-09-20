105cab341a34927af1624c54de4d733fa1f270cd
Mike Krinkin <krinkin.m.u@gmail.com>
2016-09-20 22:27:44 +0300

    add simple ticket rwlock implementation

--- kernel/inc/spinlock.h
+++ kernel/inc/spinlock.h
@@ -12,4 +12,17 @@ void spin_lock_init(struct spinlock *lock);
 unsigned long spin_lock_save(struct spinlock *lock);
 void spin_unlock_restore(struct spinlock *lock, unsigned long flags);
 
+
+struct rwlock {
+	atomic_uint write;
+	atomic_uint read;
+	atomic_uint next;
+};
+
+void rwlock_init(struct rwlock *lock);
+unsigned long read_lock_save(struct rwlock *lock);
+unsigned long write_lock_save(struct rwlock *lock);
+void read_unlock_restore(struct rwlock *lock, unsigned long flags);
+void write_unlock_restore(struct rwlock *lock, unsigned long flags);
+
 #endif /*__SPINLOCK_H__*/
--- kernel/src/spinlock.c
+++ kernel/src/spinlock.c
@@ -7,15 +7,20 @@ static void __spin_lock(struct spinlock *lock)
 	const unsigned ticket = atomic_fetch_add_explicit(&lock->next, 1,
 				memory_order_relaxed);
 
-	while (atomic_load_explicit(&lock->current, memory_order_acquire)
+	while (atomic_load_explicit(&lock->current, memory_order_relaxed)
 				!= ticket)
 		cpu_relax();
+	atomic_thread_fence(memory_order_acquire);
 }
 
 static void __spin_unlock(struct spinlock *lock)
 {
-	atomic_store_explicit(&lock->current, lock->current + 1,
-				memory_order_release);
+	const unsigned current = atomic_load_explicit(&lock->current,
+				memory_order_relaxed);
+
+	atomic_thread_fence(memory_order_release);
+	atomic_store_explicit(&lock->current, current + 1,
+				memory_order_relaxed);
 }
 
 void spin_lock_init(struct spinlock *lock)
@@ -36,6 +41,88 @@ unsigned long spin_lock_save(struct spinlock *lock)
 void spin_unlock_restore(struct spinlock *lock, unsigned long flags)
 {
 	__spin_unlock(lock);
+	if (flags & RFLAGS_IF)
+		local_int_enable();
+}
+
+void rwlock_init(struct rwlock *lock)
+{
+	atomic_init(&lock->write, 0);
+	atomic_init(&lock->read, 0);
+	atomic_init(&lock->next, 0);
+}
+
+static void __read_lock(struct rwlock *lock)
+{
+	const unsigned ticket = atomic_fetch_add_explicit(&lock->next, 1,
+				memory_order_relaxed);
+
+	while (atomic_load_explicit(&lock->read, memory_order_relaxed)
+				!= ticket)
+		cpu_relax();
+	atomic_store_explicit(&lock->read, ticket + 1, memory_order_relaxed);
+	atomic_thread_fence(memory_order_acquire);
+}
+
+static void __read_unlock(struct rwlock *lock)
+{
+	atomic_thread_fence(memory_order_release);
+	atomic_fetch_add_explicit(&lock->write, 1, memory_order_relaxed);
+}
+
+static void __write_lock(struct rwlock *lock)
+{
+	const unsigned ticket = atomic_fetch_add_explicit(&lock->next, 1,
+				memory_order_relaxed);
+
+	while (atomic_load_explicit(&lock->write, memory_order_relaxed)
+				!= ticket)
+		cpu_relax();
+	atomic_thread_fence(memory_order_acquire);
+}
+
+static void __write_unlock(struct rwlock *lock)
+{
+	const unsigned read = atomic_load_explicit(&lock->read,
+				memory_order_relaxed);
+	const unsigned write = atomic_load_explicit(&lock->write,
+				memory_order_relaxed);
+
+	atomic_thread_fence(memory_order_release);
+	atomic_store_explicit(&lock->read, read + 1,
+				memory_order_relaxed);
+	atomic_store_explicit(&lock->write, write + 1,
+				memory_order_relaxed);
+}
+
+unsigned long read_lock_save(struct rwlock *lock)
+{
+	const unsigned long flags = rflags();
+
+	local_int_disable();
+	__read_lock(lock);
+	return flags;
+}
+
+void read_unlock_restore(struct rwlock *lock, unsigned long flags)
+{
+	__read_unlock(lock);
+	if (flags & RFLAGS_IF)
+		local_int_enable();
+}
+
+unsigned long write_lock_save(struct rwlock *lock)
+{
+	const unsigned long flags = rflags();
+
+	local_int_disable();
+	__write_lock(lock);
+	return flags;
+}
+
+void write_unlock_restore(struct rwlock *lock, unsigned long flags)
+{
+	__write_unlock(lock);
 	if (flags & RFLAGS_IF)
 		local_int_enable();
 }
