6723677ab5d5b93b608bcff7a06b262c2f1f8211
Mike Krinkin <krinkin.m.u@gmail.com>
2016-09-28 19:24:03 +0300

    fix taking address of percpu varibale, probably

--- kernel/src/percpu.c
+++ kernel/src/percpu.c
@@ -14,9 +14,14 @@ void percpu_cpu_setup(void)
 	extern char percpu_phys_end[];
 
 	const size_t percpu_size = percpu_phys_end - percpu_phys_begin;
-	void *ptr = mem_alloc(percpu_size);
+	void *ptr = mem_alloc(percpu_size + sizeof(uint64_t));
 
 	BUG_ON(!ptr);
+
+	uint64_t *baseptr = (uint64_t *)((uintptr_t)ptr + percpu_size);
+	uintptr_t base = (uintptr_t)ptr + percpu_size;
+
 	memcpy(ptr, percpu_phys_begin, percpu_size);
-	write_msr(IA32_FS_BASE, (uintptr_t)ptr + percpu_size);
+	write_msr(IA32_FS_BASE, base);
+	*baseptr = base;
 }
