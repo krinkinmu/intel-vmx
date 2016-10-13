6ab098d5eeab8c43ca88dd8a96be9a104dbedcf0
Mike Krinkin <krinkin.m.u@gmail.com>
2016-10-13 14:39:33 +0300

    add missing include

--- kernel/inc/uart8250.h
+++ kernel/inc/uart8250.h
@@ -1,6 +1,8 @@
 #ifndef __UART8250_H__
 #define __UART8250_H__
 
+#include <stddef.h>
+
 void uart8250_setup(void);
 void uart8250_write(const void *src, size_t size);
 void uart8250_read(void *dst, size_t size);
