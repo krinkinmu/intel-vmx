5ae7253a63233058c8c3f8157e7f36446ae3a70b
Mike Krinkin <krinkin.m.u@gmail.com>
2016-10-24 18:33:03 +0300

    add list_insert_before/after routines

--- kernel/inc/list.h
+++ kernel/inc/list.h
@@ -14,7 +14,17 @@ struct list_head {
 
 void list_init(struct list_head *head);
 void list_add(struct list_head *new, struct list_head *head);
+
+static inline void list_insert_after(struct list_head *new,
+			struct list_head *node)
+{ list_add(new, node); }
+
 void list_add_tail(struct list_head *new, struct list_head *head);
+
+static inline void list_insert_before(struct list_head *new,
+			struct list_head *node)
+{ list_add_tail(new, node); }
+
 void list_del(struct list_head *entry);
 void list_splice(struct list_head *list, struct list_head *head);
 int list_empty(const struct list_head *head);
