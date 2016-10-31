94f671cb830ca15000bc0db9d1cc0bacea92d18a
Mike Krinkin <krinkin.m.u@gmail.com>
2016-10-31 17:09:49 +0300

    remove lock-free list test since we using hash table with locks

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -8,7 +8,6 @@
 #include <thread.h>
 #include <paging.h>
 #include <hazptr.h>
-#include <lflist.h>
 #include <debug.h>
 #include <alloc.h>
 #include <time.h>
@@ -100,66 +99,6 @@ static void test_hashtable(void)
 	printf("finished hashtable test\n");
 }
 
-struct lf_int {
-	struct lf_list_head ll;
-	int value;
-};
-
-static int lf_int_cmp(const struct lf_list_head *l,
-			const struct lf_list_head *r)
-{
-	const struct lf_int *left = (const struct lf_int *)l;
-	const struct lf_int *right = (const struct lf_int *)r;
-
-	if (left->value != right->value)
-		return left->value < right->value ? -1 : 1;
-	return 0;
-}
-
-static void __test_lflist(void *unused)
-{
-	struct mem_cache cache;
-	struct lf_list_head lst;
-	int i, j;
-
-	(void) unused;
-
-	lf_list_init(&lst);
-	mem_cache_setup(&cache, sizeof(struct lf_int), sizeof(struct lf_int));
-
-	for (i = 0; i != 10000; ++i) {
-		struct lf_int *node = mem_cache_alloc(&cache, PA_ANY);
-
-		if (!node)
-			break;
-		node->value = i;
-		BUG_ON(!lf_list_insert(&lst, &node->ll, &lf_int_cmp));
-	}
-
-	for (j = 0; j != i; ++j) {
-		struct lf_list_head *node;
-		struct lf_int key;
-
-		key.value = j;
-		BUG_ON(!(node = lf_list_lookup(&lst, &key.ll, &lf_int_cmp)));
-		BUG_ON(!lf_list_remove(&lst, &key.ll, &lf_int_cmp));
-		mem_cache_free(&cache, (struct lf_int *)node);
-	}
-
-	mem_cache_release(&cache);
-}
-
-static void test_lflist(void)
-{
-	struct thread *thread = thread_create(&__test_lflist, 0);
-
-	printf("start lock-free linked list test\n");
-	thread_activate(thread);
-	thread_join(thread);
-	thread_destroy(thread);
-	printf("finished lock-free linked list test\n");
-}
-
 void main(const struct mboot_info *info)
 {
 	gdb_hang();
@@ -186,7 +125,6 @@ void main(const struct mboot_info *info)
 	//vmx_setup();
 
 	test_hashtable();
-	test_lflist();
 
 	while (1);
 }
